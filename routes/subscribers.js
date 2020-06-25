7179241f78a8ec81f088bd84e7ecbb50e61e0932
rafael <rafaelllparaynooo23@gmail.com>
2020-06-26 01:56:32 +0800

    fix

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Subscriber = require('../models/Subscriber');
-const subscriber = require('../models/Subscriber');
-//const subscriber = require('../models/Subscriber');
-//const subscriber = require('../models/Subscriber');
 
 //getting all
 router.get('/', async (req, res) => {
